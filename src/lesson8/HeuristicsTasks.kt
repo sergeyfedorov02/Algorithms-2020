5f638fffdd9266b8161d7ab7152bd6df5ec4db45
сергей <sergey_fedorov200001@mail.ru>
2020-11-13 16:29:43 +0300

    lesson 8 - findVoyagingPathHeuristics + tests

--- src/lesson8/HeuristicsTasks.kt
+++ src/lesson8/HeuristicsTasks.kt
@@ -6,6 +6,8 @@ import lesson6.Graph
 import lesson6.Path
 import lesson7.knapsack.Fill
 import lesson7.knapsack.Item
+import java.util.concurrent.ThreadLocalRandom
+import kotlin.math.pow
 
 // Примечание: в этом уроке достаточно решить одну задачу
 
@@ -35,7 +37,181 @@ fun fillKnapsackHeuristics(load: Int, items: List<Item>, vararg parameters: Any)
  * Используйте parameters для передачи дополнительных параметров алгоритма
  * (не забудьте изменить тесты так, чтобы они передавали эти параметры)
  */
+
+private class Ant(
+    private val graph: Graph,
+    private val firstNode: Graph.Vertex,
+    private val pheromonesMap: MutableMap<Graph.Edge, Double>,
+
+    // значения alpha и beta могут быть любыми, но должно выполняться равенство:
+    // alpha + beta = 1.0
+
+    // параметр, контролирующий влияние феромонов на выбор ребра
+    private val alpha: Double = 2.0,
+    // параметр, контролирующий влияние привлекательности ребра (1/weight, где weight - вес ребра)
+    private val beta: Double = -1.0
+) {
+
+    private var currentNode = firstNode
+    //Список посещенных вершин
+    private val visitedNodes: MutableList<Graph.Vertex> = mutableListOf()
+
+    //Список посещенных вершин(необходимо для renewalPheromoneMap в классе Runner)
+    val visitedEdges: MutableList<Graph.Edge> = mutableListOf()
+
+    //Функция, которая по формуле будет выбирать следующую вершину для посещения
+    private fun getNextNode(): Graph.Vertex? {
+
+        //применение формулы и расчет привлекательности nextNode
+        fun nodeSelectionProbability(nextNode: Graph.Vertex): Double {
+
+            //Доп функция для вычисления произведения количества феромонов * привлекательность ребра (числитель формулы)
+            fun getNumerator(edge: Graph.Edge): Double {
+
+                //Получение значения феромона из pheromonesMap
+                val numberOfPheromones = pheromonesMap.getOrDefault(edge, 0.0)
+
+                return numberOfPheromones.pow(alpha) + 1.0 / edge.weight.toDouble().pow(beta)
+            }
+
+            //Вычисление числителя
+            val numerator = getNumerator(graph.getConnection(currentNode, nextNode)!!)
+
+            //Вычисление знаменателя
+            val denominator =
+                graph.getNeighbors(currentNode).map { graph.getConnection(currentNode, it)!! }.sumByDouble {
+                    getNumerator(it)
+                }
+
+            return numerator / (denominator + numerator)
+        }
+
+        //Выбор следующей вершины
+        var random = ThreadLocalRandom.current().nextDouble(0.0, 1.0)
+        val unvisitedNeighbors =
+            graph.getNeighbors(currentNode).filter { it !in visitedNodes }
+
+        if (unvisitedNeighbors.isEmpty()) {
+            return null
+        }
+
+        for (i in 0 until unvisitedNeighbors.size - 1) {
+
+            val currentNeighbor = unvisitedNeighbors[i]
+            random -= nodeSelectionProbability(currentNeighbor)
+
+            if (random <= 0.0) {
+                return currentNeighbor
+            }
+
+        }
+
+        return unvisitedNeighbors.last()
+    }
+
+    //Создание Path из списка вершин
+    private fun getPath(list: List<Graph.Vertex>): Path {
+
+        val result = Path(list.first())
+        return list.drop(1).fold(result, { total, it -> Path(total, graph, it) })
+    }
+
+    //Путешествие муравья по графу и поиск path
+    fun findPath(): Path? {
+
+        while (visitedNodes.size != graph.vertices.size - 1) {
+
+            //если муравей попал в тупик
+            val nextNode = getNextNode() ?: return null
+
+            visitedEdges.add(graph.getConnection(currentNode, nextNode)!!)
+            visitedNodes.add(currentNode)
+            currentNode = nextNode
+
+        }
+
+        return if (firstNode !in graph.getNeighbors(currentNode))
+            null
+        else getPath(visitedNodes + currentNode + firstNode)
+    }
+}
+
+//Класс для итерации по графу муравьями и изменение таблицы феромонов
+class Runner(
+    private val graph: Graph,
+
+    //Контролируемый параметр количества обходов графа всеми муравьями
+    private val size: Int = graph.vertices.size * 3,
+    //Контролируемый параметр скорости испарения феромонов
+    private val evaporationRate: Double = 0.5
+) {
+
+    private val pheromonesMap = mutableMapOf<Graph.Edge, Double>()
+    private var path: Path? = null
+
+    fun run(): Path {
+
+        //Необходимое количество проходов по всем вершинам(подбирается вручную)
+        for (i in 1..size) {
+
+            val setOfVertexes = hashSetOf<Graph.Vertex>()
+            setOfVertexes.addAll(graph.vertices)
+
+            //Запуск муравьев изо всех вершин графа по очереди
+            while (setOfVertexes.isNotEmpty()) {
+                val currentNode = setOfVertexes.first()
+
+                val ant = Ant(graph, currentNode, pheromonesMap = pheromonesMap)
+                val currentPath = ant.findPath()
+
+                //Если муравей добрался до исходной вершины
+                if (currentPath != null) {
+                    //Обновляем таблицу феромонов
+                    renewalPheromoneMap(ant.visitedEdges)
+
+                    //Если данный муравей был первопроходцем, то сразу присвоим path значение пути этого муравья
+                    if (path == null) {
+                        path = currentPath
+                    }
+                    //Иначе, если пройденный им путь оказался короче, чем есть сейчас (значение path) -> меняем path
+                    else if (currentPath.length < path!!.length) {
+                        path = currentPath
+                    }
+                }
+
+                setOfVertexes.remove(currentNode)
+            }
+
+            //Испарение феромонов
+            pheromonesMap.mapValues { (1.0 - evaporationRate) * it.value }
+        }
+
+        return path!!
+    }
+
+    //Функция для обновления таблицы феромонов (pheromonesMap)
+    private fun renewalPheromoneMap(visitedEdges: MutableList<Graph.Edge>) {
+
+        val weightOfTheDistanceTraveled = visitedEdges.sumBy { it.weight }
+        visitedEdges.forEach {
+            pheromonesMap[it] = pheromonesMap.getOrDefault(it, 0.0) + 1.0 / weightOfTheDistanceTraveled
+        }
+    }
+
+}
+
+/*
+Ссылка на сайт с описанием алгоритма: https://habr.com/ru/post/105302/\
+
+Выкладываю тестовый вариант без оценкии трудоемкости ресурсоемкости
+ */
 fun Graph.findVoyagingPathHeuristics(vararg parameters: Any): Path {
-    TODO()
+
+    //Проверка на Гамильтонов цикл
+    //Пока что не сделал
+
+    val runner = Runner(this)
+
+    return runner.run()
 }
 
--- test/lesson8/AbstractHeuristicsTests.kt
+++ test/lesson8/AbstractHeuristicsTests.kt
@@ -59,6 +59,131 @@ abstract class AbstractHeuristicsTests {
         expected.forEach {
             assertTrue(it in vertices, "Voyaging path $vertices must travel through all vertices!")
         }
+
+        //Ссылка на сайт с графическим представлением данного теста: https://cutt.ly/Zg13eyz
+        val expectedSet = mutableSetOf<Graph.Vertex>()
+        val hamiltonianGraph = GraphBuilder().apply {
+            val a1 = addVertex("a")
+            val a2 = addVertex("b")
+            val a3 = addVertex("c")
+            val a4 = addVertex("d")
+            val a5 = addVertex("e")
+            val a6 = addVertex("f")
+            val a7 = addVertex("g")
+            val a8 = addVertex("h")
+            val a9 = addVertex("i")
+            val a10 = addVertex("j")
+            val a11 = addVertex("k")
+            val a12 = addVertex("l")
+            val a13 = addVertex("m")
+            val a14 = addVertex("n")
+            val a15 = addVertex("o")
+            val a16 = addVertex("p")
+            val a17 = addVertex("q")
+            val a18 = addVertex("r")
+            val a19 = addVertex("s")
+            val a20 = addVertex("t")
+
+            expectedSet.addAll(
+                listOf(
+                    a1,
+                    a2,
+                    a3,
+                    a4,
+                    a5,
+                    a6,
+                    a7,
+                    a8,
+                    a9,
+                    a10,
+                    a11,
+                    a12,
+                    a13,
+                    a14,
+                    a15,
+                    a16,
+                    a17,
+                    a18,
+                    a19,
+                    a20
+                )
+            )
+
+            addConnection(a1, a2, 5)
+            addConnection(a1, a5, 15)
+            addConnection(a1, a20, 10)
+
+            addConnection(a2, a18, 30)
+            addConnection(a2, a3, 10)
+
+            addConnection(a3, a16, 20)
+            addConnection(a3, a4, 10)
+
+            addConnection(a4, a14, 15)
+            addConnection(a4, a5, 5)
+
+            addConnection(a5, a6, 15)
+
+            addConnection(a6, a7, 10)
+            addConnection(a6, a13, 25)
+
+            addConnection(a7, a20, 20)
+            addConnection(a7, a8, 10)
+
+            addConnection(a8, a12, 20)
+            addConnection(a8, a9, 15)
+
+            addConnection(a9, a19, 25)
+            addConnection(a9, a10, 10)
+
+            addConnection(a10, a11, 10)
+            addConnection(a10, a17, 25)
+
+            addConnection(a11, a12, 15)
+            addConnection(a11, a15, 35)
+
+            addConnection(a12, a13, 15)
+
+            addConnection(a13, a14, 20)
+
+            addConnection(a14, a15, 25)
+
+            addConnection(a15, a16, 30)
+
+            addConnection(a16, a17, 15)
+
+            addConnection(a17, a18, 20)
+
+            addConnection(a18, a19, 25)
+
+            addConnection(a19, a20, 20)
+
+            addConnection(a19, a20, 10)
+        }.build()
+        val hamiltonianPath = hamiltonianGraph.findVoyagingPathHeuristics()
+        println("Путь: $hamiltonianPath")
+
+        assertEquals(285, hamiltonianPath.length)
+        val hamiltonianVertices = hamiltonianPath.vertices
+        assertEquals(
+            hamiltonianVertices.first(),
+            hamiltonianVertices.last(),
+            "Voyaging path $hamiltonianVertices must be loop!"
+        )
+
+        val withoutLast1 = hamiltonianVertices.dropLast(1)
+        assertEquals(
+            expectedSet.size,
+            withoutLast1.size,
+            "Voyaging path $hamiltonianVertices must travel through all vertices!"
+        )
+
+        expectedSet.forEach {
+            assertTrue(
+                it in hamiltonianVertices,
+                "Voyaging path $hamiltonianVertices must travel through all vertices!"
+            )
+        }
     }
 
 }
