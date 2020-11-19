739b4cf8cd6787cfd6336cdb72d0378aebdc4aa4
сергей <sergey_fedorov200001@mail.ru>
2020-11-19 16:37:41 +0300

    lesson 5 - all tasks + test

--- src/lesson5/KtOpenAddressingSet.kt
+++ src/lesson5/KtOpenAddressingSet.kt
@@ -75,8 +75,105 @@ class KtOpenAddressingSet<T : Any>(private val bits: Int) : AbstractMutableSet<T
      *
      * Средняя
      */
+
+    /*Пусть N - размер таблицы
+
+    Трудоемкость - O(N), в худшем случае, когда element находится в самом конце множества(самый последний индекс),
+      а значение element.startingIndex() - самый первый индекс в этом множестве
+
+    Ресурсоемкость - O(1)
+    */
     override fun remove(element: T): Boolean {
-        TODO("not implemented")
+
+        //Трудоемкость - O(1)
+        if (!contains(element)) {
+            return false
+        }
+
+        //Ресурсоемкость - O(1)
+        var currentIndex = element.startingIndex()
+        var nextIndex = currentIndex
+        var nextElement = storage[nextIndex]
+
+        //Ищем позицию удаляемого объекта
+        //Трудоемкость - O(N), в худшем случае, который описан перед задачей
+        while (nextElement != element) {
+            nextIndex = (nextIndex + 1) % capacity
+            nextElement = storage[nextIndex]
+        }
+
+        //Присваиваем currentIndex значение позиции удаляемого элемента
+        currentIndex = nextIndex
+
+        //Удаляем наш element
+        storage[currentIndex] = null
+
+        //Изначальная позиция element
+        val firstPos = currentIndex
+
+        //Переприсваиваем значение следующего элемента и его индекса
+        nextIndex = (nextIndex + 1) % capacity
+        nextElement = storage[nextIndex]
+
+        //Трудоемкость - O(N), в худшем случае,
+        // если удаляем самый первый объект, а все остальные объекты идут за ним по порядку
+        //  так придется сделать полный круг
+        while (nextIndex != firstPos) {
+
+            if (nextElement == null) {
+                break
+            }
+
+            val startIndex = (nextElement as T).startingIndex()
+
+            //перенос вверх
+            if (currentIndex < nextIndex) {
+
+                //первое условие для переноса
+                if (startIndex <= currentIndex) {
+
+                    currentIndex = move(currentIndex, nextIndex)
+
+                } else {
+                    //второе условие для переноса (сдучай с загибанием)
+                    if (startIndex > nextIndex && currentIndex < startIndex) {
+
+                        currentIndex = move(currentIndex, nextIndex)
+
+                    }
+                }
+            } else {
+                //перенос вниз
+                if (startIndex == currentIndex) {
+
+                    currentIndex = move(currentIndex, nextIndex)
+
+                } else if (startIndex in (nextIndex + 1)..currentIndex) {
+                    currentIndex = move(currentIndex, nextIndex)
+                }
+            }
+
+            //Ищем элемент от следующего индекса
+            nextIndex = (nextIndex + 1) % capacity
+            nextElement = storage[nextIndex]
+
+        }
+
+        size--
+        return true
+    }
+
+    //Дополнительная функция, которая совершает перемещение двух элементов и возвращает новый currentIndex
+    private fun move(currentIndex: Int, nextIndex: Int): Int {
+
+        val nextElement = storage[nextIndex]
+        //На позицию удаляемого ставим тот, который не на своем месте
+        storage[currentIndex] = nextElement
+
+        //Перемещаем удаляемый элемент на новую позицию
+        storage[nextIndex] = null
+
+        return nextIndex
     }
 
     /**
@@ -89,7 +186,164 @@ class KtOpenAddressingSet<T : Any>(private val bits: Int) : AbstractMutableSet<T
      *
      * Средняя (сложная, если поддержан и remove тоже)
      */
-    override fun iterator(): MutableIterator<T> {
-        TODO("not implemented")
+
+    /*
+    Пусть N - размер таблицы
+    Тогда для итератора:
+
+        Трудоемкость - O(N), в худшем случае, которые описаны для каждого метода в их реализации:
+                                1) При использовании next и hasNext будет использоваться updateNextIndex
+                                2) При использовании Iterator.remove, будет использован remove
+
+        Ресурсоемкость - O(N), в худшем случае, так как надо хранить listOfReviewedElements (подробнее написано ниже)
+     */
+    override fun iterator(): MutableIterator<T> =
+        OpenAddressingSetIterator()
+
+    inner class OpenAddressingSetIterator internal constructor() : MutableIterator<T> {
+
+        private var nextIndex = 0
+        private var currentIndex = -1
+
+        //Если элемент уже был рассмотрен ранее, то этот флаг изменится, чтобы при вызове next правильно определить значение
+        private var containsFlag = false
+
+        //Если при удалении элемента на его позиции не оказался другой элемент -> этот флаг изменится
+        private var notNullElementFlag = false
+
+        //Флаг для проверки в Iterator.remove, чтобы не было вызвано удаление два раза подряд
+        private var removeFlag = false
+
+        /*Так как Iterator.remove использует remove, а там возможны перемещения элементов
+          То может произойти ситуация, что ранее рассмотренный элемент переместиться вниз(ниже currentIndex) и будет рассмотрен дважды
+          Для предотвращения таких ситуаций заведем список элементов, которые могут переместиться вниз
+
+          Реусорсоемкость - O(N) в худшем случае, когда все у всех элементов startingIndex больше их текущего индекса
+
+          */
+        private val listOfReviewedElements = mutableSetOf<T>()
+
+        init {
+            updateNextIndex()
+        }
+
+        //Трудоемкоть - O(N), в худшем случае, когда бежим с нулевого индекса и до capacity
+
+        //Функция для обновления следующего индекса во множестве
+        private fun updateNextIndex() {
+
+            //После первого изменения currentIndex у нас storage[nextIndex] будет != null -> следующий цикл с while не запуститься
+            if (currentIndex != -1) {
+                nextIndex++
+            }
+
+            while (nextIndex < capacity && storage[nextIndex] == null) {
+                nextIndex++
+            }
+
+        }
+
+        /**
+         * Проверка наличия следующего элемента
+         *
+         * Функция возвращает true, если итерация по множеству ещё не окончена (то есть, если вызов next() вернёт
+         * следующий элемент множества, а не бросит исключение); иначе возвращает false.
+         */
+
+        //Трудоемкость - O(N), в худшем случае, так как тут мы используем updateNextIndex
+        // и может быть такая ситуация, что в таблице всего 1 элемент на нулевой позиции -> будем бежать до capacity
+        override fun hasNext(): Boolean {
+
+            //Если было удаление, то найдем index текущего элемента
+            val index = if (notNullElementFlag) {
+                currentIndex // если storage[currentIndex] != null
+            } else nextIndex //В обратном случае -> возьмем следующий элемент
+
+            //Проверка, был ли рассмотрен ранее этот элемент
+            if (nextIndex < capacity && listOfReviewedElements.contains(storage[index])) {
+                //Если removeFlag == true -> index = currentIndex, Но currentIndex != nextIndex,
+                //тогда при вызове updateNextIndex() мы потеряем 1 элемент
+                nextIndex = index
+                updateNextIndex()
+                currentIndex = nextIndex
+                containsFlag = true
+            }
+
+            return nextIndex < capacity
+        }
+
+        /**
+         * Получение следующего элемента
+         *
+         * Функция возвращает следующий элемент множества.
+         *
+         * Бросает IllegalStateException, если все элементы уже были возвращены.
+         */
+
+        /*
+        Трудоемкость - O(N), так как используем updateNextIndex, подробнее случай описан в hasNext
+         */
+        override fun next(): T {
+
+            if (nextIndex >= capacity) {
+                throw IllegalStateException()
+            }
+
+            //Если элемент уже был рассмотрен -> в hasNext мы уже поменяли currentIndex
+            if (containsFlag) {
+                containsFlag = false
+
+            } else {
+
+                if (!notNullElementFlag) {
+                    currentIndex = nextIndex
+                } else {
+                    nextIndex = currentIndex
+                }
+            }
+
+            removeFlag = false
+            notNullElementFlag = false
+            val element = storage[currentIndex]
+            updateNextIndex()
+
+            //Если у элемента startingIndex > currentIndex -> он после вызова remove может переметситься вниз, добдавляем такой элемент
+            if ((element as T).startingIndex() > currentIndex) {
+                listOfReviewedElements.add(element)
+            }
+
+            return element
+        }
+
+
+        /**
+         * Удаление предыдущего элемента
+         *
+         * Функция удаляет из множества элемент, возвращённый крайним вызовом функции next().
+         *
+         * Бросает IllegalStateException, если функция была вызвана до первого вызова next() или же была вызвана
+         * более одного раза после любого вызова next().
+         */
+
+        //Трудоемксоть - O(N) в худшем случае, так как используем remove
+        override fun remove() {
+
+            check(currentIndex != -1 && !removeFlag)
+
+            //Трудоемксоть - O(N), в худшем случае (случай описан в реализации remove)
+            remove(storage[currentIndex]!!)
+
+            removeFlag = true
+
+            //Если удаляем последний элемент во множестве -> currentIndex не меняется, а в hasNext будет false
+            if (nextIndex < capacity) {
+
+                //Если при удалении элемента на его месте оказывается null-> меняем currentIndex
+                if (storage[currentIndex] == null) {
+                    currentIndex = nextIndex
+                } else notNullElementFlag = true
+            }
+
+        }
     }
 }
--- test/lesson5/AbstractOpenAddressingSetTest.kt
+++ test/lesson5/AbstractOpenAddressingSetTest.kt
@@ -40,6 +40,210 @@ abstract class AbstractOpenAddressingSetTest {
     }
 
     protected fun doRemoveTest() {
+
+        //Мои тесты
+        for (iteration in 1..9) {
+            val controlSet = mutableSetOf<String>()
+            var bitsNumber: Int
+            var openAddressingSet: MutableSet<String>
+            var removeElement: String
+            when (iteration) {
+
+                1 -> {
+                    bitsNumber = 5
+                    openAddressingSet = create(bitsNumber)
+                    removeElement = "cb"
+
+                    openAddressingSet.add("c3bc1")
+                    openAddressingSet.add("cc")
+                    openAddressingSet.add("cb")
+                    openAddressingSet.add("32")
+                    openAddressingSet.add("c2b3c")
+                    openAddressingSet.add("b")
+
+                    controlSet.add("c2b3c")
+                    controlSet.add("c3bc1")
+                    controlSet.add("cc")
+                    controlSet.add("32")
+                    controlSet.add("b")
+
+                }
+
+                2 -> {
+                    bitsNumber = 5
+                    openAddressingSet = create(bitsNumber)
+                    removeElement = "32acb"
+
+                    openAddressingSet.add("a")
+                    openAddressingSet.add("32acb")
+                    openAddressingSet.add("bc")
+                    openAddressingSet.add("2ba31")
+                    openAddressingSet.add("a1c31")
+                    openAddressingSet.add("3bb")
+
+                    controlSet.add("a1c31")
+                    controlSet.add("a")
+                    controlSet.add("3bb")
+                    controlSet.add("bc")
+                    controlSet.add("2ba31")
+                }
+
+                3 -> {
+                    bitsNumber = 3
+                    openAddressingSet = create(bitsNumber)
+                    removeElement = "2c"
+
+                    openAddressingSet.add("acaa2")
+                    openAddressingSet.add("23")
+                    openAddressingSet.add("2c")
+                    openAddressingSet.add("1cb3")
+                    openAddressingSet.add("233a3")
+                    openAddressingSet.add("c")
+
+                    controlSet.add("233a3")
+                    controlSet.add("23")
+                    controlSet.add("acaa2")
+                    controlSet.add("1cb3")
+                    controlSet.add("c")
+                }
+
+                4 -> {
+                    bitsNumber = 3
+                    openAddressingSet = create(bitsNumber)
+                    removeElement = "2c"
+
+                    openAddressingSet.add("2c")
+                    openAddressingSet.add("cba")
+                    openAddressingSet.add("c")
+                    openAddressingSet.add("c3ca1")
+                    openAddressingSet.add("b3c")
+                    openAddressingSet.add("a1133")
+
+                    controlSet.add("c")
+                    controlSet.add("c3ca1")
+                    controlSet.add("cba")
+                    controlSet.add("b3c")
+                    controlSet.add("a1133")
+                }
+
+                5 -> {
+                    bitsNumber = 6
+                    openAddressingSet = create(bitsNumber)
+                    removeElement = "a"
+
+                    openAddressingSet.add("a1")
+                    openAddressingSet.add("a")
+                    openAddressingSet.add("222")
+                    openAddressingSet.add("1c")
+                    openAddressingSet.add("2")
+                    openAddressingSet.add("3a3c")
+
+                    controlSet.add("222")
+                    controlSet.add("1c")
+                    controlSet.add("2")
+                    controlSet.add("a1")
+                    controlSet.add("3a3c")
+                }
+
+                6 -> {
+                    bitsNumber = 2
+                    openAddressingSet = create(bitsNumber)
+                    removeElement = "1"
+
+                    openAddressingSet.add("0")
+                    openAddressingSet.add("2")
+                    openAddressingSet.add("1")
+                    openAddressingSet.add("3")
+
+                    controlSet.add("2")
+                    controlSet.add("0")
+                    controlSet.add("3")
+
+                }
+
+                7 -> {
+                    bitsNumber = 5
+                    openAddressingSet = create(bitsNumber)
+                    removeElement = "cb"
+
+                    openAddressingSet.add("c3bc1")
+                    openAddressingSet.add("cc")
+                    openAddressingSet.add("cb")
+                    openAddressingSet.add("32")
+                    openAddressingSet.add("33")
+                    openAddressingSet.add("b")
+
+                    controlSet.add("33")
+                    controlSet.add("c3bc1")
+                    controlSet.add("cc")
+                    controlSet.add("32")
+                    controlSet.add("b")
+
+                }
+
+                8 -> {
+                    bitsNumber = 4
+                    openAddressingSet = create(bitsNumber)
+                    removeElement = "cb"
+
+                    openAddressingSet.add("23")
+                    openAddressingSet.add("bc")
+                    openAddressingSet.add("c")
+                    openAddressingSet.add("1aca")
+                    openAddressingSet.add("cb")
+                    openAddressingSet.add("c1")
+
+                    controlSet.add("23")
+                    controlSet.add("1aca")
+                    controlSet.add("c")
+                    controlSet.add("c1")
+                    controlSet.add("bc")
+                }
+
+                else -> {
+                    bitsNumber = 6
+                    openAddressingSet = create(bitsNumber)
+                    removeElement = "1"
+
+                    openAddressingSet.add("23")
+                    openAddressingSet.add("2b11")
+                    openAddressingSet.add("2c")
+                    openAddressingSet.add("22b3b")
+                    openAddressingSet.add("1")
+                    openAddressingSet.add("2")
+
+                    controlSet.add("23")
+                    controlSet.add("2b11")
+                    controlSet.add("2c")
+                    controlSet.add("22b3b")
+                    controlSet.add("2")
+                }
+            }
+
+            assertTrue(
+                openAddressingSet.remove(removeElement),
+                "An element wasn't removed contrary to expected."
+            )
+            assertFalse(
+                removeElement in openAddressingSet,
+                "A supposedly removed element is still in the set."
+            )
+
+            for (element in controlSet) {
+                assertTrue(
+                    openAddressingSet.contains(element),
+                    "Open addressing set doesn't have the element $element from the control set."
+                )
+            }
+            controlSet.clear()
+
+            assertFalse(
+                openAddressingSet.remove(removeElement),
+                "A removed element was supposedly removed twice."
+            )
+
+        }
+
         val random = Random()
         for (iteration in 1..100) {
             val bitsNumber = random.nextInt(4) + 6
@@ -121,6 +325,193 @@ abstract class AbstractOpenAddressingSetTest {
     }
 
     protected fun doIteratorRemoveTest() {
+
+        //Мои тесты
+        for (iteration in 1..8) {
+            var bitsNumber: Int
+            val controlSet = mutableSetOf<String>()
+            var toRemove: String
+
+            when (iteration) {
+
+                //Удаление предпоследнего элемента коллекции с перемещение последнего на его позицию
+                1 -> {
+                    toRemove = "32b3c"
+                    bitsNumber = 3
+
+                    controlSet.add("b211")
+                    controlSet.add("1")
+                    controlSet.add("13")
+                    controlSet.add("3")
+                    controlSet.add("32b3c")
+                    controlSet.add("a3aa")
+                }
+
+                //Удаление последнего элемента с перемещение элемента сверху на его позицию
+                2 -> {
+                    toRemove = "caab"
+                    bitsNumber = 7
+
+                    controlSet.add("c1aa3")
+                    controlSet.add("2")
+                    controlSet.add("c")
+                    controlSet.add("babaa")
+                    controlSet.add("caab")
+                    controlSet.add("c23c")
+                }
+
+                //Удаление элемента с перемещением вниз, уже рассмотренного элемента
+                3 -> {
+                    toRemove = "4bh8a611d3b7a"
+                    bitsNumber = 4
+
+                    controlSet.add("1")
+                    controlSet.add("2")
+                    controlSet.add("1d4e3dc8cbea")
+                    controlSet.add("daa7cg")
+                    controlSet.add("ce5eb1e25")
+                    controlSet.add("ahe62a")
+                    controlSet.add("4bh8a611d3b7a")
+                    controlSet.add("4f4d5agd")
+                    controlSet.add("aha")
+                    controlSet.add("b58hde7161ef7")
+                    controlSet.add("75cf7286e36")
+                    controlSet.add("2afba5hc31hfd")
+                    controlSet.add("gffd7ge72hd")
+                    controlSet.add("72344h")
+                    controlSet.add("hd")
+
+                }
+
+                //просто удаление элемента без перемещния других
+                4 -> {
+                    toRemove = "2a36c152"
+                    bitsNumber = 9
+
+                    controlSet.add("2c6b77")
+                    controlSet.add("1b7a")
+                    controlSet.add("2a36c152")
+                    controlSet.add("3c2a2a")
+                    controlSet.add("ca3a6a3")
+                    controlSet.add("b1517bc")
+                    controlSet.add("61a77c2c")
+                    controlSet.add("3b7c71")
+                }
+
+                //удаление предпоследнего без перемещения других
+                5 -> {
+                    toRemove = "1bc11"
+                    bitsNumber = 5
+
+                    controlSet.add("b1ac3")
+                    controlSet.add("22a13")
+                    controlSet.add("b1332")
+                    controlSet.add("3b")
+                    controlSet.add("1bc11")
+                    controlSet.add("3bb")
+                }
+
+                //удаление элемента с перемещением элементов, лежащих ниже
+                6 -> {
+                    toRemove = "b2675"
+                    bitsNumber = 4
+
+                    controlSet.add("bc213322")
+                    controlSet.add("2177a")
+                    controlSet.add("7cac1")
+                    controlSet.add("b2675")
+                    controlSet.add("abc115")
+                    controlSet.add("6")
+                    controlSet.add("51b1a")
+                    controlSet.add("cba6")
+                }
+
+                //Удаление предпоследнего с перемещение на его место уже рассмотренного выше элемента
+                7 -> {
+                    toRemove = "63a888a"
+                    bitsNumber = 4
+
+                    controlSet.add("58d")
+                    controlSet.add("7767dhegdf417")
+                    controlSet.add("7eg82")
+                    controlSet.add("d")
+                    controlSet.add("75cg233bd")
+                    controlSet.add("h2623e157c")
+                    controlSet.add("cc5b86ca")
+                    controlSet.add("g3ec")
+                    controlSet.add("f1b2")
+                    controlSet.add("ag175ac")
+                    controlSet.add("hga85abhb2e")
+                    controlSet.add("63a888a")
+                    controlSet.add("a3a6b8c5")
+                    controlSet.add("5ab1cd5")
+                    controlSet.add("f7h2")
+                }
+
+                //Удаляем элемент и на его место перемещается элемент уже рассмотренный ранее
+                else -> {
+                    toRemove = "5a"
+                    bitsNumber = 4
+
+                    controlSet.add("e42h7g843c")
+                    controlSet.add("e6e32")
+                    controlSet.add("8b737dg3eba7f")
+                    controlSet.add("828feb8edh")
+                    controlSet.add("hf5a")
+                    controlSet.add("65g3")
+                    controlSet.add("5a")
+                    controlSet.add("e2bfg3")
+                    controlSet.add("bf83h6321b2bbc")
+                    controlSet.add("76d25aa545")
+                    controlSet.add("e77a")
+                    controlSet.add("67g61fhghdd564")
+                    controlSet.add("186ba")
+                    controlSet.add("ea726111bb127")
+                    controlSet.add("gh31ee4")
+                }
+            }
+
+            val openAddressingSet = create<String>(bitsNumber)
+            for (element in controlSet) {
+                openAddressingSet += element
+            }
+            controlSet.remove(toRemove)
+            val iterator = openAddressingSet.iterator()
+            assertFailsWith<IllegalStateException>("Something was supposedly deleted before the iteration started") {
+                iterator.remove()
+            }
+
+            var counter = openAddressingSet.size
+            while (iterator.hasNext()) {
+                val element = iterator.next()
+                counter--
+                if (element == toRemove) {
+                    iterator.remove()
+                }
+            }
+            assertEquals(
+                0, counter,
+                "OpenAddressingSetIterator.remove() changed iterator position: ${abs(counter)} elements were ${if (counter > 0) "skipped" else "revisited"}."
+            )
+            assertEquals(
+                controlSet.size, openAddressingSet.size,
+                "The size of the set is incorrect: was ${openAddressingSet.size}, should've been ${controlSet.size}."
+            )
+            for (element in controlSet) {
+                assertTrue(
+                    openAddressingSet.contains(element),
+                    "Open addressing set doesn't have the element $element from the control set."
+                )
+            }
+            for (element in openAddressingSet) {
+                assertTrue(
+                    controlSet.contains(element),
+                    "Open addressing set has the element $element that is not in control set."
+                )
+            }
+            controlSet.clear()
+        }
+
         val random = Random()
         for (iteration in 1..100) {
             val controlSet = mutableSetOf<String>()
